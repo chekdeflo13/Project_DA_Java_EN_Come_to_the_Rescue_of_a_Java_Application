663762859b8a25630abfbdd546d343540c2dd441
Florence Herment <chekdeflo13@hotmail.com>
2021-12-18 19:08:30 +0100

    method occurrenceCount created

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,6 +1,7 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  * describe symptoms and count their occurrences
@@ -17,5 +18,27 @@ public class AnalyticsCounter {
 		// create an ordered list from symptoms.txt file and read all lines.
 		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(PATH_INPUT_FILE);
 		List<String> symptoms = reader.getSymptoms();
+
+		// count symptoms occurrences and ordered them into a list
+		TreeMap<String, Long> orderedCount = new TreeMap<>();
+		occurrenceCount(symptoms, orderedCount);
+
+	}
+
+	/**
+	 * count symptoms occurrences in this method.
+	 * 
+	 * @param symptoms     = List of symptoms
+	 * @param orderedCount = Count occurrences and ordered them
+	 */
+	private static void occurrenceCount(List<String> symptoms, TreeMap<String, Long> orderedCount) {
+		for (String line : symptoms) {
+			if (orderedCount.containsKey(line)) {
+				orderedCount.put(line, orderedCount.get(line) + 1);
+			} else {
+				orderedCount.put(line, (long) 1L);
+			}
+		}
 	}
 }
+
